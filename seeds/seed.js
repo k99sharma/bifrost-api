023c6b9c085f24636c560b8408184a754b6a217c
Kalash <kalash.strt@gmail.com>
2021-05-29 11:45:22 +0530

    seed file is updated to get fetch data of every country

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,33 @@
 // seeding the database
 const Country = require('../modals/country');
 
+// importing modules
 const fetch = require('node-fetch');
+const mongoose = require('mongoose');
 
-// extracting data 
-const extractCountryData = async (data)=>{
-    // countryData = {
-    //     name : data[0].name,
-    //     capital : data[0].capital,
-    //     region : data[0].region,
-    //     subregion : data[0].subregion,
-    //     population : data[0].population,
-    //     area : data[0].area,
-    //     nativeName : data[0].nativeName,
-    //     flag : data[0].flag
-    // }
+
+// country code array
+const countryCode = ["CO", "IN"];
+
+// connecting to database
+mongoose.connect('mongodb://localhost:27017/bifrost', 
+{
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+  .then(()=>{
+    console.log('Database connection successful !');
+  })
+  .catch(err => {
+    console.log('Database connection failed !');
+    console.log(err);
+  })
+
+
+  let totalCountriesSaved = 0;
+
+// extracting data and saving in DB
+const extractAndSaveCountryData = async (data)=>{
     const countryData = new Country({
         name : data[0].name,
         capital : data[0].capital,
@@ -28,24 +41,35 @@ const extractCountryData = async (data)=>{
         languages : data[0].languages,
         timezones : data[0].timezones,
         callingCodes : data[0].callingCodes
-    });
+    })
 
-    console.log(countryData);
+    await countryData.save()
+        .then(()=>{
+            console.log('Country is successfully saved');
+            totalCountriesSaved += 1;
+        })
+        .catch(err => {
+            console.log('Unable to save country');
+            console.log('Error: ' + err);
+        })
 }
+
 
 // function to fetch country data 
 const fetchCountry = async ()=>{
-    await fetch(
-        'https://restcountries.eu/rest/v2/alpha?codes=CO'
-    )
-        .then(res => res.json())
-        .then(data => {
-            console.log('Fetch is successful');
-            extractCountryData(data);
-        })
-        .catch(err => {
-            console.log('Error: ' + err);
-        })
+    countryCode.map(async code => {
+        await fetch(
+            `https://restcountries.eu/rest/v2/alpha?codes=${code}`
+        )
+            .then(res => res.json())
+            .then(data => {
+                console.log('Fetch is successful');
+                extractAndSaveCountryData(data);
+            })
+            .catch(err => {
+                console.log('Error: ' + err);
+            })
+    });    
 }
 
 fetchCountry();
