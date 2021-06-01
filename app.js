1b34474937d015e04b4e98d02886fe1af43a1547
Kalash <kalash.strt@gmail.com>
2021-06-01 22:36:55 +0530

    cross site scripting prevention is added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 
 // importing routes
@@ -19,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json({ limit : "10kb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
 
 
 // database connection
