931643e92505ca1ec9366132736018276ce14974
Kalash <kalash.strt@gmail.com>
2021-06-01 22:26:03 +0530

    route url is updated with versoning

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ mongoose.connect('mongodb://localhost:27017/bifrost',
 
 
 // routes
-app.use('/bifrost', fetchCountry);
+app.use('/bifrost/v1.0/', fetchCountry);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
