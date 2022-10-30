f768254f2e712d01ef99c9147561ebfcb914c636
Kalash Sharma <kalash.strt@gmail.com>
2022-10-31 02:06:56 +0530

    Update fetchCountry.js

--- controllers/fetchCountry.js
+++ controllers/fetchCountry.js
@@ -13,7 +13,7 @@ exports.getCountryData = async (req, res) => {
         code : countryCode
     });
 
-    console.log(country)
+    console.log(country + " " + countryCode)
     if(!country){
         return res.status(404).send({ error : "Data not found"});
     }
